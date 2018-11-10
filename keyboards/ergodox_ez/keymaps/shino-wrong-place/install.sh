432fade1e3fb5fef8267751a1e510d0973da1cc9
Shunichi Shinohara <shino@shiguredo.jp>
2018-11-10 10:34:38 +0900

    Add launchctl unload/load for karabiner-elements' agent

--- keyboards/ergodox_ez/keymaps/shino-wrong-place/install.sh
+++ keyboards/ergodox_ez/keymaps/shino-wrong-place/install.sh
@@ -4,6 +4,9 @@ set -ex
 
 make ergodox_ez:shino-wrong-place
 
+launchctl unload -w /Library/LaunchAgents/org.pqrs.karabiner.karabiner_console_user_server.plist
 
 teensy_loader_cli -vvv -mmcu=atmega32u4 -w ergodox_ez_shino-wrong-place.hex
+
+launchctl load -w /Library/LaunchAgents/org.pqrs.karabiner.karabiner_console_user_server.plist
 
