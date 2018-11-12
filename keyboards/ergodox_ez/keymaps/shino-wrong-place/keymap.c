c778e2c91e38635a5d06886dde7593df0d6dce8c
Shunichi Shinohara <shino@shiguredo.jp>
2018-11-12 18:22:41 +0900

    Misc changes

--- keyboards/ergodox_ez/keymaps/shino-wrong-place/keymap.c
+++ keyboards/ergodox_ez/keymaps/shino-wrong-place/keymap.c
@@ -20,19 +20,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *       ,--------------------------------------------------.    ,--------------------------------------------------.
  *       | Tab  |  1 ! |  2 @ |  3 # |  4 $ |  5 % |  ESC   |    | RESET  |  6 ^ |  7 & |  8 * |  9 ( |  0 ) | ESC  |
  *       |------+------+------+------+------+------+--------|    |--------+------+------+------+------+-------------|
- *       |      |   Q  |   W  |   E  |   R  |   T  |  INT1  |    |   DEL  |   Y  |   U  |   I  |   O  |   P  |      |
+ *       |      |   Q  |   W  |   E  |   R  |   T  |  LAlt  |    |  RGui  |   Y  |   U  |   I  |   O  |   P  |      |
  *       |  =+  |------+------+------+------+------+--------|    |--------+------+------+------+------+------|  [{  |
- *       |------|   A  |   S  |   D  |   F  |   G  |  INT2  |    |   BS   |   H  |   J  |   K  |   L  |  ;:  |------|
+ *       |------|   A  |   S  |   D  |   F  |   G  |  INT1  |    |   BS   |   H  |   J  |   K  |   L  |  ;:  |------|
  *       |  -_  |------+------+------+------+------+--------|    |--------+------+------+------+------+------|  }]  |
- *       |      |   Z  |   X  |   C  |   V  |   B  |  Grv~  |    |   \|   |   N  |   M  |  ,<  |  .>  |  /?  |      |
+ *       |      |   Z  |   X  |   C  |   V  |   B  |  Grv~  |    |   B    |   N  |   M  |  ,<  |  .>  |  /?  |      |
  *       `-------------+------+------+------+------+--------'    `--------+------+------+------+------+-------------'
  *                     | Grv~ | LAlt | LGui | LCtl |LShift|        |RShift| SPC  |Enter | '"   |  \|  |
  *                     `----------------------------------'        `----------------------------------'
  *    ,--------------.                                                                                  ,-------------.
- *    | Mute  | Tab  |                                                                                  | LEFT | RIGHT |
+ *    | Mute  | Play |                                                                                  | LEFT | RIGHT |
  *    |-------+------+------.                                                                   ,-------|------|-------|
  *    | VolUp |      |      |                                                                   |       |      |   Up  |
- *    |-------| Play |  SPC |                                                                   | Enter | RGui |-------|
+ *    |-------|  NO  |  SPC |                                                                   | Enter |  NO  |-------|
  *    | VolDn |      |      |                                                                   |       |      |  Down |
  *    `---------------------'                                                                   `----------------------'
  */
@@ -41,23 +41,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
                   // MY RIGHT (Normally left)
                   RESET,       KC_6,   KC_7,   KC_8,     KC_9,    KC_0,      KC_ESC,
-                  KC_DEL,      KC_Y,   KC_U,   KC_I,     KC_O,    KC_P,      KC_LBRC,
+                  KC_RGUI,     KC_Y,   KC_U,   KC_I,     KC_O,    KC_P,      KC_LBRC,
                   KC_BSPC,     KC_H,   KC_J,   KC_K,     KC_L,    KC_SCLN,
-                  KC_BSLS,     KC_N,   KC_M,   KC_COMM,  KC_DOT,  KC_SLSH,   KC_RBRC,
+                  KC_B,        KC_N,   KC_M,   KC_COMM,  KC_DOT,  KC_SLSH,   KC_RBRC,
                   KC_RSFT,     KC_SPC, KC_ENT, KC_QUOT,  KC_BSLS,
                                                                                       KC_LEFT, KC_RIGHT,
                                                                                                KC_UP,
-                                                                              KC_ENT, KC_RGUI, KC_DOWN,
+                                                                              KC_ENT, KC_NO,   KC_DOWN,
 
           // MY LEFT (Normally right)
           KC_TAB,         KC_1,         KC_2,   KC_3,    KC_4,    KC_5,    KC_ESC,
-          KC_EQL,         KC_Q,         KC_W,   KC_E,    KC_R,    KC_T,    KC_INT1,
-                          KC_A,         KC_S,   KC_D,    KC_F,    KC_G,    KC_INT2,
+          KC_EQL,         KC_Q,         KC_W,   KC_E,    KC_R,    KC_T,    KC_LALT,
+                          KC_A,         KC_S,   KC_D,    KC_F,    KC_G,    KC_INT1,
           KC_MINS,        KC_Z,         KC_X,   KC_C,    KC_V,    KC_B,    KC_GRV,
                                         KC_GRV, KC_LALT, KC_LGUI, KC_LCTL, KC_LSFT,
-     KC_MUTE,  KC_TAB,
+     KC_MUTE,  KC_MPLY,
      KC_VOLU,
-     KC_VOLD,  KC_MPLY, KC_SPC
+     KC_VOLD,  KC_NO, KC_SPC
     ),
 
 /* Keymap 1: Symbol Layer
