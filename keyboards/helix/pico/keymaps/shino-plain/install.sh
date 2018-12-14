0d8ceb0b7dacfc6f53ade2630382d48c8983b4d4
Shunichi Shinohara <shino@shiguredo.jp>
2018-12-14 14:16:02 +0900

    Use avrdude and wait for RESET pressed

--- keyboards/helix/pico/keymaps/shino-plain/install.sh
+++ keyboards/helix/pico/keymaps/shino-plain/install.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
-set -ex
+set -e
+# set -x
 
-make helix/pico:shino-plain
+# make helix/pico:shino-plain
+# make helix/pico:shino-plain:avrdude
+docker run -e keymap=shino-plain -e keyboard=helix/pico --rm -v $('pwd'):/qmk:rw edasque/qmk_firmware
 
-teensy_loader_cli -vvv -mmcu=atmega32u4 -w helix/pico_shino-plain.hex
+MCU=atmega32u4
+BUILD_DIR=.
+TARGET=helix_pico_shino-plain
+
+GREP=grep
+
+USB= ;
+
+printf "Detecting USB port, reset your controller now.";
+while [ -z "${USB}" ]; do
+    sleep 0.5;
+    printf ".";
+    USB=`ls /dev/tty.usb*; true`;
+done;
+echo "";
+echo "Detected controller on USB port at ${USB}";
+sleep 1;
+
+# avrdude -p  -c avr109 -P /dev/tty.usbmodem1411 -U flash:w:helix_pico_shino-plain.hex
+
+avrdude -p ${MCU} -c avr109 -P ${USB} -U flash:w:${BUILD_DIR}/${TARGET}.hex; \
 
