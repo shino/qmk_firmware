bec196fab5b97db67e67753cc2c766d65c392456
Shunichi Shinohara <shino@shiguredo.jp>
2018-12-16 11:27:20 +0900

    Additional comment

--- keyboards/helix/pico/keymaps/shino-plain/keymap.c
+++ keyboards/helix/pico/keymaps/shino-plain/keymap.c
@@ -67,7 +67,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  |   ,  |   .  |   /  |Enter |
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
-   * |Adjust| Esc  | Alt  | GUI  | EISU |Lower |Space |Space |Raise | KANA | Left | Down |  Up  |Right |
+   * |Adjust| Up   | Down |Lower |Space |Space | LGui | RAlt |Space | Riase| RAlt | Left |Right |RGui  |
+   * |      |      |      |      |      |      | EISUU| KANA |      |      |      |      |      |      |
    * `-------------------------------------------------------------------------------------------------'
    */
 
