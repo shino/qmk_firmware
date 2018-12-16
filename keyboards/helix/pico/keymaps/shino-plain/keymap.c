3119bdf5e47d446c0259035bf5c030fac0a2a5d0
Shunichi Shinohara <shino@shiguredo.jp>
2018-12-16 11:44:22 +0900

    Move B to the left

--- keyboards/helix/pico/keymaps/shino-plain/keymap.c
+++ keyboards/helix/pico/keymaps/shino-plain/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Ctrl |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |  '   |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  |   ,  |   .  |   /  |Enter |
+   * | Shift|   Z  |   X  |   C  |   V  | / ?  |             |   B  |   N  |   M  |  , < |  . > |Enter |
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
    * |Adjust| Up   | Down |Lower |Space |Space | LGui | RAlt |Space | Riase| RAlt | Left |Right |RGui  |
    * |      |      |      |      |      |      | EISUU| KANA |      |      |      |      |      |      |
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT( \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_SLSH,                   KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_ENT , \
       ADJUST,  KC_UP,   KC_DOWN, LOWER,   KC_SPC,  KC_SPC,  KC_LGUI, KC_RALT, KC_SPC,  RAISE,   KC_RALT, KC_RGUI, KC_LEFT, KC_RGHT \
       ),
   /* Colemak
