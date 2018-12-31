fc5be04e22b62603680aa2d41460104866da1a4d
Shunichi Shinohara <shino@shiguredo.jp>
2018-12-31 15:05:27 +0900

    mod-tap for D with RAISE

--- keyboards/helix/pico/keymaps/shino/keymap.c
+++ keyboards/helix/pico/keymaps/shino/keymap.c
@@ -59,6 +59,9 @@ enum macro_keycodes {
 #define F_LS LSFT_T(KC_F)
 #define J_RS RSFT_T(KC_J)
 
+#define D_RA LT(_RAISE, KC_D)
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty
@@ -75,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT( \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-      KC_LCTL, KC_A,    KC_S,    KC_D,    F_LS,    KC_G,                      KC_H,    J_RS,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+      KC_LCTL, KC_A,    KC_S,    D_RA,    F_LS,    KC_G,                      KC_H,    J_RS,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
       ADJUST,  KC_ESC,  KC_LALT, KC_LGUI, EISU,    LOWER,   KC_SPC,  KC_SPC,  RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
       ),
