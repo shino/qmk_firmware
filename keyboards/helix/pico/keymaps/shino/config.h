1c061d2ba9bb01d9d87b45c191f385476ad4d7a8
Shunichi Shinohara <shino@shiguredo.jp>
2019-01-04 14:51:15 +0900

    Change mouse key settings

--- keyboards/helix/pico/keymaps/shino/config.h
+++ keyboards/helix/pico/keymaps/shino/config.h
@@ -25,7 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Slow key tapper
 #undef TAPPING_TERM
-#define TAPPING_TERM 500
+#define TAPPING_TERM 200
 
 // Use key repeat for keys with long tap
 #undef TAPPING_FORCE_HOLD
@@ -33,6 +33,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // https://docs.qmk.fm/#/feature_advanced_keycodes?id=permissive-hold
 #define IGNORE_MOD_TAP_INTERRUPT
 
+// mouse key settings
+#define MOUSEKEY_DELAY             50
+#define MOUSEKEY_INTERVAL          50
+#define MOUSEKEY_MAX_SPEED         10
+#define MOUSEKEY_TIME_TO_MAX       10
+#define MOUSEKEY_WHEEL_MAX_SPEED    8
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 40
 
 #ifdef AUDIO_ENABLE
   #define STARTUP_SONG SONG(STARTUP_SOUND)
