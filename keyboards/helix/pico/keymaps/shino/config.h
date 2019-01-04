d785803e307616edb4ef885b1d96a4bdaeaf14b4
Shunichi Shinohara <shino@shiguredo.jp>
2019-01-04 14:28:08 +0900

    Enable mouse keys, actually

--- keyboards/helix/pico/keymaps/shino/config.h
+++ keyboards/helix/pico/keymaps/shino/config.h
@@ -33,9 +33,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // https://docs.qmk.fm/#/feature_advanced_keycodes?id=permissive-hold
 #define IGNORE_MOD_TAP_INTERRUPT
 
-// Use Mouse keys
-#define MOUSE_KEY_ENABLE yes
-
 
 #ifdef AUDIO_ENABLE
   #define STARTUP_SONG SONG(STARTUP_SOUND)
