ebbbc1f9f6ecae5b6560b35f8973b0c00610f765
Shunichi Shinohara <shino@shiguredo.jp>
2019-03-10 10:25:22 +0900

    Add ascii only text file

---
+++ keyboards/helix/pico/keymaps/shino2/extract-ascii.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR=./keyboards/helix/pico/keymaps/shino2/
+
+SRC=${DIR}/keymap.c
+OUT=${DIR}/ascii.txt
+
+for layer in Mouse Symbol "Shots Right" "Shots Left" CurNum EucalynX; do
+    grep -E "\/\* ${layer}" ${SRC} |
+        sed -e 's/ *\/\* //'
+    grep -A 9 -E "\/\* ${layer}" ${SRC} | grep -v "\/\* " |
+        sed -e 's/ *\* /  /'
+    echo
+done |
+    tee ${OUT}
