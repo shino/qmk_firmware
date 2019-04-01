bc6539202d2151dd8f0022daebb76f772bb5b124
Shunichi Shinohara <shino@shiguredo.jp>
2019-04-01 18:22:31 +0900

    Update ascii map

--- keyboards/helix/pico/keymaps/shino2/extract-ascii.sh
+++ keyboards/helix/pico/keymaps/shino2/extract-ascii.sh
@@ -9,7 +9,7 @@ for layer in Mouse Symbol "Shots Right" "Shots Left" CurNum EucalynX; do
     grep -E "\/\* ${layer}" ${SRC} |
         sed -e 's/ *\/\* //'
     grep -A 9 -E "\/\* ${layer}" ${SRC} | grep -v "\/\* " |
-        sed -e 's/ *\* /  /'
+        sed -e 's/ *\* //'
     echo
 done |
     tee ${OUT}
