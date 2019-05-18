735220806d498878963d2de3a1a22b18c5896122
Shunichi Shinohara <shino@shiguredo.jp>
2019-05-18 09:25:55 +0900

    Current ascii text

--- keyboards/helix/pico/keymaps/shino3/extract-ascii.sh
+++ keyboards/helix/pico/keymaps/shino3/extract-ascii.sh
@@ -6,7 +6,7 @@ TO_CLIPBOARD=pbcopy
 SRC=${DIR}/keymap.c
 OUT=${DIR}/ascii.txt
 
-for layer in Adjust Coffee Symbol Extras Stars EucalynX; do
+for layer in Adjust Coffee Stars EucalynX; do
     grep -E "\/\* ${layer}" ${SRC} |
         sed -e 's/ *\/\* //'
     grep -A 9 -E "\/\* ${layer}" ${SRC} | grep -v "\/\* " |
