a5ba72660e69762080eea7a930b7aeb495cbabf7
Shunichi Shinohara <shino@shiguredo.jp>
2019-07-08 18:27:21 +0900

    Assign keys for , and .

--- keyboards/helix/pico/keymaps/shino3/extract-ascii.sh
+++ keyboards/helix/pico/keymaps/shino3/extract-ascii.sh
@@ -6,7 +6,7 @@ TO_CLIPBOARD=pbcopy
 SRC=${DIR}/keymap.c
 OUT=${DIR}/ascii.txt
 
-for layer in Adjust Coffee Symbol FinCol; do
+for layer in Adjust Extra Symbol FinCol; do
     grep -E "\/\* ${layer}" ${SRC} |
         sed -e 's/ *\/\* //'
     grep -A 9 -E "\/\* ${layer}" ${SRC} | grep -v "\/\* " |
