738635dd297260f221ef6e1fa18e18887dd158e1
Shunichi Shinohara <shino@shiguredo.jp>
2019-12-06 21:08:32 +0900

    Change ascii text order

--- keyboards/helix/pico/keymaps/shino3/extract-ascii.sh
+++ keyboards/helix/pico/keymaps/shino3/extract-ascii.sh
@@ -10,7 +10,7 @@ TO_CLIPBOARD="xsel -b"
 SRC=${DIR}/keymap.c
 OUT=${DIR}/ascii.txt
 
-for layer in Adjust Extra Symbol FinCol; do
+for layer in FinCol Symbol Extra Adjust; do
     grep -E "\/\* ${layer}" ${SRC} |
         sed -e 's/ *\/\* //'
     grep -A 9 -E "\/\* ${layer}" ${SRC} | grep -v "\/\* " |
