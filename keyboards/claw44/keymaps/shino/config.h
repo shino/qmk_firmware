6619a5c6f6ef86fe6bc1c09fcbd17ee28c747ac0
Shunichi Shinohara <shino@shiguredo.jp>
2020-07-13 09:52:48 +0900

    Tune mouse parametets

--- keyboards/claw44/keymaps/shino/config.h
+++ keyboards/claw44/keymaps/shino/config.h
@@ -42,10 +42,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define IGNORE_MOD_TAP_INTERRUPT
 
 // mouse key settings
-#define MOUSEKEY_DELAY             50
-#define MOUSEKEY_INTERVAL          50
-#define MOUSEKEY_MAX_SPEED         10
-#define MOUSEKEY_TIME_TO_MAX       10
+#define MOUSEKEY_DELAY             10
+#define MOUSEKEY_INTERVAL          33
+#define MOUSEKEY_MAX_SPEED         40
+#define MOUSEKEY_TIME_TO_MAX       30
 #define MOUSEKEY_WHEEL_DELAY       50
 #define MOUSEKEY_WHEEL_INTERVAL    30
 #define MOUSEKEY_WHEEL_MAX_SPEED    8
