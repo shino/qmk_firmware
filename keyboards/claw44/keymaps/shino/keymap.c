c3b9a265d46d0fe16d0b2e300d8737601abf2351
Shunichi Shinohara <shino@shiguredo.jp>
2020-08-03 09:26:32 +0900

    misc

--- keyboards/claw44/keymaps/shino/keymap.c
+++ keyboards/claw44/keymaps/shino/keymap.c
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|------+------+------+------+------+------|   |------+------+------+------+------+------|
       VOLD ,  V   ,  Z   ,  W   ,  X   , MNXT ,     DOWN ,  H   ,  J   ,  K   ,  L   , LEFT ,
   //`------+------+------+------+------+------'   `------+------+------+------+------+------'
-                    ESsc , LAal , O_ct , ENsf ,     JAsf , SPsy , C_ss , BSgu
+                    ENsf , LAal , O_ct , ENsf ,     JAsf , SPsy , C_ss , JAsf
   //              `------+------+------+------'   `------+------+------+------'
   ),
 
