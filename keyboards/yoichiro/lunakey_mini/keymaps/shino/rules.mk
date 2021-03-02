d9485a4b57d85718f404c4f5a9900c015d3a7eea
Shunichi Shinohara <shino@shiguredo.jp>
2021-03-02 13:09:15 +0900

    Remove update tri layer call

--- keyboards/yoichiro/lunakey_mini/keymaps/shino/rules.mk
+++ keyboards/yoichiro/lunakey_mini/keymaps/shino/rules.mk
@@ -2,5 +2,5 @@ RGBLIGHT_ENABLE = no         # Enable keyboard RGB Underglow
 AUDIO_ENABLE = no            # Enable Audio output
 OLED_DRIVER_ENABLE = no      # Enable OLED Display
 
-# EXTRAKEY_ENABLE = yes
-# MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+MOUSEKEY_ENABLE = yes
