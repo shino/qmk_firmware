9cd9818b269e6995964c2fddebfc73041dcc9af1
Shunichi Shinohara <shino@shiguredo.jp>
2021-03-03 09:49:47 +0900

    Set LED by custom keycodes

--- keyboards/yoichiro/lunakey_mini/keymaps/shino/keymap.c
+++ keyboards/yoichiro/lunakey_mini/keymaps/shino/keymap.c
@@ -24,9 +24,11 @@ enum layer_number {
 
 enum custom_keycodes {
   FINCOL = SAFE_RANGE,
-  SYMBOL,
+  SYMBOL_ON,
+  SYMBOL_OFF,
   S_SYMB,
-  EXTRA
+  EXTRA_ON,
+  EXTRA_OFF
 };
 
 // common
@@ -55,8 +57,10 @@ enum custom_keycodes {
 #define KC_R_sf LSFT_T(KC_R)
 
 // Toggle layers
-#define KC__ex   TG(_EXTRA)
-#define KC__sy   TG(_SYMBOL)
+#define KC_ex   EXTRA_ON
+#define KC_exx  EXTRA_OFF
+#define KC_sy   SYMBOL_ON
+#define KC_syx  SYMBOL_OFF
 #define KC__ss   TG(_S_SYMB)
 
 
@@ -67,9 +71,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_FINCOL] = LAYOUT_kc(
   //,------+------+------+------+------+------.   ,------+------+------+------+------+------.
-      VOLU ,  Q   ,  D   ,  G   ,  C   , MPLY ,      UP,    B   ,  N   ,  P   ,  F   , _sy  ,
+      VOLU ,  Q   ,  D   ,  G   ,  C   , MPLY ,      UP,    B   ,  N   ,  P   ,  F   ,  sy  ,
   //|------+------+------+------+------+------|   |------+------+------+------+------+------|
-      VOLD , A_sf ,  I   ,  U   ,  E   , _ex  ,      ENT ,  M   ,  S   ,  T   , R_sf , RGHT ,
+      VOLD , A_sf ,  I   ,  U   ,  E   ,  ex  ,      ENT ,  M   ,  S   ,  T   , R_sf , RGHT ,
   //|------+------+------+------+------+------|   |------+------+------+------+------+------|
       WBAK ,  V   ,  X   ,  W   ,  Y   , MNXT ,     DOWN ,  H   ,  J   ,  K   ,  L   , LEFT ,
   //`------+------+------+------+------+------'   `------+------+------+------+------+------'
@@ -79,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_SYMBOL] = LAYOUT_kc( \
   //,------+------+------+------+------+------.   ,------+------+------+------+------+------.
-           , BSLS ,  7   ,  8   ,  9   ,      ,          , DQUO , LBRC , RBRC , ENT  , _sy  ,
+           , BSLS ,  7   ,  8   ,  9   ,      ,          , DQUO , LBRC , RBRC , ENT  , syx  ,
   //|------+------+------+------+------+------|   |------+------+------+------+------+------|
            ,  0   ,  4   ,  5   ,  6   ,      ,          , MINS , COMM , DOT  , SLSH ,      ,
   //|------+------+------+------+------+------|   |------+------+------+------+------+------|
@@ -105,7 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,------+------+------+------+------+------.   ,------+------+------+------+------+------.
            ,      ,      ,      ,      ,      ,          , WH_L , WH_D , WH_U , WH_R ,      ,
   //|------+------+------+------+------+------|   |------+------+------+------+------+------|
-           ,      ,      ,      ,      , _ex  ,          , MS_L , MS_D , MS_U , MS_R ,      ,
+           ,      ,      ,      ,      , exx  ,          , MS_L , MS_D , MS_U , MS_R ,      ,
   //|------+------+------+------+------+------|   |------+------+------+------+------+------|
            ,      ,      ,      ,      ,      ,          , LEFT , DOWN ,  UP  , RGHT ,      ,
   //`------+------+------+------+------+------'   `------+------+------+------+------+------'
@@ -115,40 +119,64 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 
-const rgblight_segment_t PROGMEM my_fincol_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 12, 0,0,0}
-);
-const rgblight_segment_t PROGMEM my_sy_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 12, HSV_PURPLE}
-);
-const rgblight_segment_t PROGMEM my_ex_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 12, HSV_RED}
-);
+/* #ifdef RGBLIGHT_ENABLE */
+/* const rgblight_segment_t PROGMEM my_fincol_layer[] = RGBLIGHT_LAYER_SEGMENTS( */
+/*     {0, 12, 0,0,0} */
+/* ); */
+/* const rgblight_segment_t PROGMEM my_sy_layer[] = RGBLIGHT_LAYER_SEGMENTS( */
+/*     {0, 12, HSV_PURPLE} */
+/* ); */
+/* const rgblight_segment_t PROGMEM my_ex_layer[] = RGBLIGHT_LAYER_SEGMENTS( */
+/*     {0, 12, HSV_RED} */
+/* ); */
 
-const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-    my_fincol_layer,
-    my_sy_layer,
-    my_ex_layer
-);
+/* const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST( */
+/*     my_fincol_layer, */
+/*     my_sy_layer, */
+/*     my_ex_layer */
+/* ); */
+/* #endif */
 
 void keyboard_post_init_user(void) {
-    // Enable the LED layers
-    rgblight_layers = my_rgb_layers;
+#ifdef RGBLIGHT_ENABLE
+  rgblight_sethsv_noeeprom(0, 0, 0);
+#endif
 }
 
-/* bool led_update_user(led_t led_state) { */
-/*     rgblight_set_layer_state(0, led_state.caps_lock); */
-/*     return true; */
-/* } */
-
-/* layer_state_t default_layer_state_set_user(layer_state_t state) { */
-/*     rgblight_set_layer_state(1, layer_state_cmp(state, _SYMBOL)); */
-/*     return state; */
-/* } */
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-    rgblight_set_layer_state(0, layer_state_cmp(state, _FINCOL));
-    rgblight_set_layer_state(1, layer_state_cmp(state, _SYMBOL));
-    rgblight_set_layer_state(2, layer_state_cmp(state, _EXTRA));
-    return state;
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+  case SYMBOL_ON:
+    if (record->event.pressed) {
+      layer_on(_SYMBOL);
+#ifdef RGBLIGHT_ENABLE
+      rgblight_setrgb(0x00,  0x00, 0xFF);
+#endif
+    }
+    return false;
+  case SYMBOL_OFF:
+    if (record->event.pressed) {
+      layer_off(_SYMBOL);
+#ifdef RGBLIGHT_ENABLE
+      rgblight_setrgb(0x00,  0x00, 0x00);
+#endif
+    }
+    return false;
+  case EXTRA_ON:
+    if (record->event.pressed) {
+      layer_on(_EXTRA);
+#ifdef RGBLIGHT_ENABLE
+      rgblight_setrgb(0x00,  0xFF, 0x00);
+#endif
+    }
+    return false;
+  case EXTRA_OFF:
+    if (record->event.pressed) {
+      layer_off(_EXTRA);
+#ifdef RGBLIGHT_ENABLE
+      rgblight_setrgb(0x00,  0x00, 0x00);
+#endif
+    }
+    return false;
+  }
+  return true;
 }
